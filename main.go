33989489e622d5b52a31d1e6ce8e1dc3a9032a80
nubesFilius <eperezlarosa@outlook.com>
2024-09-22 16:19:01 -0400

    add_go_as_default_search_language

--- main.go
+++ main.go
@@ -7,44 +7,46 @@ import (
 	"os"
 )
 
+const defaultLanguage = "go"
+
 func main() {
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
 	// StackExchange API calls
-	seAllTimeQuestions, err := externalapi.SEGetTotalQuestions("go")
+	seAllTimeQuestions, err := externalapi.SEGetTotalQuestions(defaultLanguage)
 	if err != nil {
 		log.Fatal("Failed to get total StackExchange total questions:", err)
 	}
-	log.Println("StackExchange total questions to day:", seAllTimeQuestions)
+	log.Println("StackExchange total questions to date:", seAllTimeQuestions)
 
-	seAllQuestionsLastMonth, err := externalapi.SEGetTotalQuestionsFromLastMonth("go")
+	seAllQuestionsLastMonth, err := externalapi.SEGetTotalQuestionsFromLastMonth(defaultLanguage)
 	if err != nil {
 		log.Fatal("Failed to get total StackExchange total questions last month:", err)
 	}
 	log.Println("StackExchange total questions last month:", seAllQuestionsLastMonth)
 
-	seAllQuestionsThisMonth, err := externalapi.SEGetTotalQuestionsThisMonth("go")
+	seAllQuestionsThisMonth, err := externalapi.SEGetTotalQuestionsThisMonth(defaultLanguage)
 	if err != nil {
 		log.Fatal("Failed to get total StackExchange total questions this month:", err)
 	}
 	log.Println("StackExchange total questions since beginning of month:", seAllQuestionsThisMonth)
 
 	// GitHub REST API Calls
-	ghReposCreatedAllTime, err := externalapi.GHGetReposCreatedAllTime("go")
+	ghReposCreatedAllTime, err := externalapi.GHGetReposCreatedAllTime(defaultLanguage)
 	if err != nil {
-		log.Fatal("Failed to get github repos created all time:", err)
+		log.Fatal("Failed to get github repos created to date:", err)
 	}
-	log.Println("Github repos created in total:", ghReposCreatedAllTime)
+	log.Println("Github repos created in to date:", ghReposCreatedAllTime)
 
-	ghReposCreatedThisMonth, err := externalapi.GHGetReposCreatedThisMonth("go")
+	ghReposCreatedThisMonth, err := externalapi.GHGetReposCreatedThisMonth(defaultLanguage)
 	if err != nil {
 		log.Fatal("Failed to get github repos created this month:", err)
 	}
 	log.Println("Github repos created this month:", ghReposCreatedThisMonth)
 
-	ghReposCreatedLastMonth, err := externalapi.GHGetReposCreatedLastMonth("go")
+	ghReposCreatedLastMonth, err := externalapi.GHGetReposCreatedLastMonth(defaultLanguage)
 	if err != nil {
 		log.Fatal("Failed to get github repos created last month:", err)
 	}
